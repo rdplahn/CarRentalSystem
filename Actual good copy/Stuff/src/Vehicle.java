4f9867aeaac43e32c1d3fe734caf772e237996c0
rdplahn <041402459@my.central.wa.edu.au>
2014-05-01 09:27:38 +0800

    Upload commented class files

--- Actual good copy/Stuff/src/Vehicle.java
+++ Actual good copy/Stuff/src/Vehicle.java
@@ -17,6 +17,7 @@ public class Vehicle {
 	 * Class constructor
 	 */
 	
+	// RP default constructor
 		public Vehicle() {
 		this.manufacturer = "Central";
 		this.model = "ITWEB";
@@ -52,10 +53,12 @@ public class Vehicle {
 	/**
 	 * Prints details for {@link Vehicle}
 	 */
+	 
+	 //RP simply prints the details of the vehicle
 	public void printDetails() {
 		System.out.println("Vehicle: " + manufacturer + " " + model + " (" + makeYear + " model)");
 		System.out.println(journey.getKilometers() + "kms travelled, raising $" + profit);
-		journey.printLastServiceAt();
+		journey.printLastServiceAt();//calls a method in the journey object to determine and print the last service
 		System.out.println("\n\n");
 	}
 
@@ -65,50 +68,60 @@ public class Vehicle {
 	 * @param distance
 	 *            distance of kilometers traveled
 	 */
+	 
+	 //RP (not used -- previous version)vehicles can have kilometers added. Method is in vehicle class to ensure kilometers are added to the corresponding journey
 	public void addKilometers(double distance) {
 		journey.addKilometers(distance);
 	}
 
 	public void serviceVehicle()
 	{
+		//RP calls the service method in the service object to create a service, and ensures that the journey has a record of the service as well
 		service.serviceVehicle();
 		journey.servicedAt();
 	}
 	
+	//RP requests to rent the vehicle. From the main function, a rental is called THROUGH the vehicle object, 
+	//RP to ensure that the rental is related to the specific car, rather than a random rental
 	public void PerDayRental(double kilometers, int days)
 	{
+		// RP call the dueForService function to check if the vehicle is over the service KM limit
 		if (service.dueForService())
 		{
+			//RP if so, do not allow the vehicle to be rented. Alert the user
 			System.out.println("This vehicle is due for a service, and cannot be rented");
 			System.out.println("\n\n");
 		}
 		else
 		{
+			//RP if not due for a service, add a new rental to the PerDayRental object. Add the kilometers to the journey.
 			perDayRental.newRental(kilometers, days);
 			journey.addKilometers(kilometers);
+			//RP request the profit from the current PerDayRental, and add it to past profit. This total profit is a property of the VEHICLE,
+			//RP so that it can be tracked across both PerKmRentals and PerDayRentals
 			this.profit = this.profit + perDayRental.getProfit();
 			service.addKilometers(kilometers);
 		}
 	}
 	
-	//requests to rent the vehicle. From the main function, a rental is called THROUGH the vehicle object, 
-	//to ensure that the rental is related to the specific car, rather than a random rental
+	//RP requests to rent the vehicle. From the main function, a rental is called THROUGH the vehicle object, 
+	//RP to ensure that the rental is related to the specific car, rather than a random rental
 	public void PerKmRental(double kilometers)
 	{
-		// call the dueForService function to check if the vehicle is over the service KM limit
+		// RP call the dueForService function to check if the vehicle is over the service KM limit
 		if (service.dueForService())
 			{
-			//if so, do not allow the vehicle to be rented. Alert the user
+			//RP if so, do not allow the vehicle to be rented. Alert the user
 			System.out.println("This vehicle is due for a service, and cannot be rented");
 			System.out.println("\n\n");
 			}
 		else
 			{
-			//if not due for a service, add a new rental to the rental object. Add the kilometers to the journey.
+			//RP if not due for a service, add a new rental to the PerKmRental object. Add the kilometers to the journey.
 			perKmRental.newRental(kilometers);
 			journey.addKilometers(kilometers);
-			//request the profit from the current rental, and add it to past profit. This total profit is a property of the VEHICLE,
-			//so that it can be tracked across both PerKmRentals and PerDayRentals
+			//RP request the profit from the current PerKmRental, and add it to past profit. This total profit is a property of the VEHICLE,
+			//RP so that it can be tracked across both PerKmRentals and PerDayRentals
 			this.profit = this.profit + perKmRental.getProfit();
 			service.addKilometers(kilometers);
 			}
